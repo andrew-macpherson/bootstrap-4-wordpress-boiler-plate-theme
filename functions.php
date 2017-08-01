269e77cbc0f7b76e503670f9820fb0b2dc770079
andrew-macpherson <andrewmac17@gmail.com>
2017-08-01 10:18:03 -0400

    common element

--- functions.php
+++ functions.php
@@ -69,3 +69,61 @@ class bootstrap_Walker extends Walker_Nav_Menu {
 
 	}
 }
+
+
+
+
+
+
+
+//// COMMON ELEMENTS POST TYPE
+add_action( 'init', 'create_common_elements_post_types' );
+function create_common_elements_post_types() {
+    register_post_type( 'common-elements',
+        array(
+            'labels' => array(
+                'name' => 'Common Elements',
+                'singular_name' => 'Common Element',
+                'add_new' => 'Add New',
+                'add_new_item' => 'Add New Common Element',
+                'edit' => 'Edit',
+                'edit_item' => 'Edit Common Element',
+                'new_item' => 'New Common Element',
+                'view' => 'View',
+                'view_item' => 'View Common Element',
+                'search_items' => 'Search Common Elements',
+                'not_found' => 'No Common Elements found',
+                'not_found_in_trash' => 'No Common Elements found in Trash'
+            ),
+            'public' => true,
+            'menu_position' => 5,
+            'supports' => array( 'title', 'editor'),
+            'taxonomies' => array( '' ),
+            'menu_icon' => '',
+            'has_archive' => false,
+            'rewrite' => array( 'slug' => 'common-elements', 'with_front' => false ),
+        )
+    );
+}
+
+//// COMMON ELEMENSTS SHORT CODE
+function common_element($atts,$content){
+	extract( shortcode_atts( array(
+        'id' 		=> '0'
+    ), $atts ) );
+
+    $content = do_shortcode( shortcode_unautop( $content ) );
+    $content = stripParagraphs($content);
+
+    $return = '';
+
+    if($id != 0){
+		$post = get_post($id); 
+		$content = apply_filters('the_content',$post->post_content);
+		$content = do_shortcode($content);
+		$return .= $content;
+    }
+
+	return $return;
+}
+add_shortcode('common_element','common_element');
